a9a2a5d372f7131c483c6d0d3bad5de359f6158e
Rohit kumar <112196544+0Rohit2022@users.noreply.github.com>
2022-10-01 21:40:12 +0530

    Add files via upload

---
+++ js/scirpt.js
@@ -0,0 +1,19 @@
+const textareaEl = document.getElementById('textarea');
+
+const totalcounterEl = document.getElementById('total-counter');
+
+const remainingcounterEl = document.getElementById('remaining-counter');
+
+
+
+
+textareaEl.addEventListener("keyup", () => {
+    updateTextarea()
+})
+
+updateTextarea()
+
+function updateTextarea() {
+   totalcounterEl.innerText = textareaEl.value.length;
+   remainingcounterEl.innerText = textareaEl.getAttribute("maxLength")-textareaEl.value.length;
+}
